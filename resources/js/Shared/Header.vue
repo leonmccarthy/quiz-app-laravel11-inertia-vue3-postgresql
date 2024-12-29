0595421526732847e6cccfdeaf27c63b2fd235c1
leonmccarthy <leonmccarthy05@gmail.com>
2024-12-29 17:48:24 +0300

    Added app name to header from environment file

--- resources/js/Shared/Header.vue
+++ resources/js/Shared/Header.vue
@@ -1,8 +1,17 @@
 <script setup>
-    import { Link } from '@inertiajs/vue3';
+    import { Link, usePage } from '@inertiajs/vue3';
+    import { computed } from 'vue';
+
+    const page = usePage();
+    const appName = computed(()=>page.props.appName);
 </script>
 <template>
     <header class="d-flex justify-content-center py-3">
+      <div class="col-md-3 mb-2 mb-md-0">
+        <Link class="d-inline-flex link-body-emphasis" href="/">
+          <span class="fs-4">{{ appName }}</span>
+        </Link>
+      </div>
       <ul class="nav nav-pills">
         <li class="nav-item"><Link href="/" class="nav-link active" aria-current="page">Home</Link></li>
         <li class="nav-item"><Link href="/quiz" class="nav-link">Quiz</Link></li>
