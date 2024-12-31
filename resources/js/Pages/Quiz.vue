95ee682052c403eb2a1dbdcf30ab61ff1c36f677
leonmccarthy <leonmccarthy05@gmail.com>
2024-12-31 20:45:35 +0300

    Added selecting options and going to the next question

--- resources/js/Pages/Quiz.vue
+++ resources/js/Pages/Quiz.vue
@@ -9,29 +9,58 @@
     const currentQuestion = computed(()=>{
         return props.questions[currentIndex.value];
     });
+
     const answers = computed(()=>{
         return props.questions[currentIndex.value].answers;
     });
+
+    const totalQuestions = computed(()=>props.questions.length);
+    const lastQuestion = computed(()=>currentIndex.value+1==totalQuestions.value);
+
+    const selectedAnswer = ref(null);
+    const result = ref(null);
+    const selectedOption = (index)=>{
+        selectedAnswer.value = index;
+        alert(selectedAnswer.value);
+    }
+
+    const nextQuestion = ()=>{
+        if(selectedAnswer.value==null){
+            alert('Please select an answer');
+        }else{
+            if(props.questions[currentIndex.value].answers[selectedAnswer.value].correct_answer==true){
+                result.value++;
+            }
+            currentIndex.value++;
+            selectedAnswer.value=null;            
+        }
+    }
+
 </script>
 
 <template>
     <Layout>
+        <div class="d-flex justify-content-center">Question {{ currentIndex +1 }} out of {{ totalQuestions  }}</div>
         <div class="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-center">
             <div class="list-group">
-                <a href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                <a  class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
                 <div class="d-flex gap-2 w-100 justify-content-between">
                     <div>
                     <h6 class="mb-0">{{ currentQuestion.question }}</h6>
                     </div>
                 </div>
                 </a>
-                <a  v-for="(answer, index) in answers" href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                <a @click="selectedOption(index)"  v-for="(answer, index) in answers" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
                 <div class="d-flex gap-2 w-100 justify-content-between">
                     <div>
                     <p class="mb-0 opacity-75">{{ answer.answers }}</p>
                     </div>
                 </div>
                 </a>
+                <div class="d-flex justify-content-around p-2">
+                    <button @click="nextQuestion" v-if="!lastQuestion" class="btn btn-primary mx-1">Next</button>
+                    <button v-if="lastQuestion" class="btn btn-success mx-1">Submit</button>
+                </div>
             </div>
         </div>
     </Layout>
